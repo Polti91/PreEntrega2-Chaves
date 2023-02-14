22e0bff5b7eaada6199c6411bdb2129b88213da8
Pablo Chaves <chavespablo91@gmail.com>
2023-02-14 13:30:11 -0300

    Agregué detalles finales, json, asincronía y limpié el código

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,40 @@
+//Declaramos variables globales
+let arrayPresupuestos = [];
+let arraySistemas = [];
+let sistema = document.getElementById("sistema");
+let tablets = document.getElementById("tablets");
+let integracion = document.getElementById("integracion");
+let pcAdicional = document.getElementById("pcAd");
+let abono = document.getElementById("mesAbono");
+let nombre = document.getElementById("nombre");
+let calcular = document.getElementById("calcular");
+let presupuestoNro = 0;
+let valorDeTablet = 0;
+let valorDeIntegracion = 0;
+//Buscamos el historial de presupuestos
+let listar = document.getElementById("listar");
+let historial = document.getElementById("historialPresupuestos");
+//Capturamos para el botón Eliminar
+let eliminar = document.getElementById("eliminar");
+//Capturamos para el botón VER
+let ver = document.getElementById("ver");
+//Capturamos boton restear para borrar todo
+let reset = document.getElementById("resetear");
+
+//Si hay presupuestos en el LocalStorage, los traemos, sino, la dejamos vacía
+if (localStorage.getItem("presupuestos")) {
+  arrayPresupuestos = JSON.parse(localStorage.getItem("presupuestos"));
+}
+
+//Creamos un array con el precio del abono mensual para 3 6 9 y 12 meses
+const arrayAbono = [9000, 17000, 25000, 34000];
+
+//Agregamos un Pop-up "publicitario"
+
+setTimeout(()=>{
+  Swal.fire('¡Recordá que podés recibir atención personalizada comunicándote con nosotros vía telefónica!')
+}, 10000)
+
 //Constructor de objetos(presupuestos)
 class presupuestar {
   constructor(
@@ -28,43 +65,39 @@ class presupuestar {
     this.pcAdicional = pcAdicional;
   }
 }
-//Establecemos los valores de los sistemas y módulos como objetos
-const sistemaNg = {
-  sistema: "NG",
-  precio: 20000,
-  tablets: true,
-  precioTablets: 5000,
-  integracion: true,
-  precioIntegracion: 5000,
-  mensualidad: 3000,
-  pcAdicional: 2000,
-};
-const sistemaG1 = {
-  sistema: "G1",
-  precio: 15000,
-  tablets: "No disponible",
-  precioTablets: 0,
-  integracion: "No disponible",
-  precioIntegracion: 0,
-  mensualidad: 3000,
-  pcAdicional: 1800,
-};
-
-//Creamos un array con el precio del abono mensual para 3 6 9 y 12 meses
-const arrayAbono = [9000, 17000, 25000, 34000];
-let arrayPresupuestos = [];
-
-let sistema = document.getElementById("sistema");
-let tablets = document.getElementById("tablets");
-let integracion = document.getElementById("integracion");
-let pcAdicional = document.getElementById("pcAd");
-let abono = document.getElementById("mesAbono");
-let nombre = document.getElementById("nombre");
-let calcular = document.getElementById("calcular");
-let presupuestoNro = 0;
-let valorDeTablet = 0;
-let valorDeIntegracion = 0;
-
+//Constructor de objetos(sistemas)
+class generarSistema {
+  constructor(
+    sistema,
+    precio,
+    tablets,
+    precioTablets,
+    integracion,
+    precioIntegracion,
+    mensualidad,
+    pcAdicional
+  ) {
+    this.sistema = sistema;
+    this.precio = precio;
+    this.tablets = tablets;
+    this.precioTablets = precioTablets;
+    this.integracion = integracion;
+    this.precioIntegracion = precioIntegracion;
+    this.mensualidad = mensualidad;
+    this.pcAdicional = pcAdicional;
+  }
+}
+//Traemos la información de los sistemas del json
+const generarSistemas = async () => {
+  const response = await fetch("../sistemas.json")
+  const data = await response.json()
+  //Los creamos como objetos y guardamos en un Array
+  for(let sistem of data){
+    let agregarSistema = new generarSistema(sistem.sistema, sistem.precio, sistem.tablets, sistem.precioTablets, sistem.integracion, sistem.precioIntegracion, sistem.mensualidad, sistem.pcAdicional)
+    arraySistemas.push(agregarSistema)
+  }
+}
+generarSistemas()
 
 //Si elegimos G1, se deshabilitan los select de los módulos que el G1 no tiene
 sistema.addEventListener("click", () => {
@@ -85,60 +118,50 @@ sistema.addEventListener("click", () => {
   }
 });
 
-
-
 calcular.addEventListener("click", () => {
   calcularPresupuesto();
 });
 
-
-
-
 function calcularPresupuesto() {
   //Establecemos el número del presupuesto
-  if (localStorage.getItem("numeroPresupuesto")){
+  if (localStorage.getItem("numeroPresupuesto")) {
     presupuestoNro = parseInt(localStorage.getItem("numeroPresupuesto"));
   }
   presupuestoNro = presupuestoNro + 1;
-  localStorage.setItem("numeroPresupuesto", presupuestoNro)
-  //tomamos valores de los input
+  localStorage.setItem("numeroPresupuesto", presupuestoNro);
+  //Tomamos valores de los input
   nombre = document.getElementById("nombre");
   pcAdicional = document.getElementById("pcAd");
   abono = document.getElementById("mesAbono");
   sistema = document.getElementById("sistema");
   tablets = document.getElementById("tablets");
   integracion = document.getElementById("integracion");
-  //revisamos si hay modulo de tablets e integración. Si no hay, guarda el valor 0 en el precio de cada una
-  tablets.value == "1" ? valorDeTablet = 0 : valorDeTablet = sistemaNg.precioTablets;
-  // if (tablets.value == "1") {
-  //   valorDeTablet = 0;
-  // } else {
-  //   valorDeTablet = sistemaNg.precioTablets;
-  // }
-  
-  integracion.value == "1" ? valorDeIntegracion = 0 : valorDeIntegracion = sistemaNg.precioIntegracion;
-  // if (integracion.value == "1") {
-  //   valorDeIntegracion = 0;
-  // } else {
-  //   valorDeIntegracion = sistemaNg.precioIntegracion;
-  // }
 
-  
-  //revisamos que hayamos puesto un nombre
+  //Revisamos si hay modulo de tablets e integración. Si no hay, guarda el valor 0 en el precio de cada una
+
+  tablets.value == "1"
+    ? (valorDeTablet = 0)
+    : (valorDeTablet = arraySistemas[0].precioTablets);
+  integracion.value == "1"
+    ? (valorDeIntegracion = 0)
+    : (valorDeIntegracion = arraySistemas[0].precioIntegracion);
+
+  //Revisamos que hayamos puesto un nombre
   if (nombre.value == "") {
     Swal.fire({
       icon: "error",
       title: "Oops...",
       text: "Debe ingresar un nombre.",
     });
-    //acá ejecutamos todo el calculo
+
+    //Acá ejecutamos todo el calculo
   } else if (sistema.value == "NG") {
     //Guardamos el presupuesto en un objeto y dentro de un array.
     const presupuesto1 = new presupuestar(
       presupuestoNro,
       nombre.value,
       sistema.value,
-      sistemaNg.precio,
+      arraySistemas[0].precio,
       tablets[tablets.value].innerText,
       valorDeTablet,
       integracion[integracion.value].innerText,
@@ -146,95 +169,78 @@ function calcularPresupuesto() {
       abono[abono.value].innerText,
       arrayAbono[abono.value],
       pcAdicional.value,
-      sistemaNg.pcAdicional
+      arraySistemas[0].pcAdicional
     );
     arrayPresupuestos.push(presupuesto1);
-    //agregar funcion que muestre el presu
+    //Mostramos el presupuesto
     mostrarPresupuesto(presupuesto1);
   } else {
     const presupuesto1 = new presupuestar(
       presupuestoNro,
       nombre.value,
       sistema.value,
-      sistemaG1.precio,
-      sistemaG1.tablets,
-      sistemaG1.precioTablets,
-      sistemaG1.integracion,
-      sistemaG1.precioIntegracion,
+      arraySistemas[1].precio,
+      arraySistemas[1].tablets,
+      arraySistemas[1].precioTablets,
+      arraySistemas[1].integracion,
+      arraySistemas[1].precioIntegracion,
       abono[abono.value].innerText,
       arrayAbono[abono.value],
       pcAdicional.value,
-      sistemaG1.pcAdicional
+      arraySistemas[1].pcAdicional
     );
     arrayPresupuestos.push(presupuesto1);
 
-    //agregar funcion que muestre el presu
+    //Mostramos el presupuesto
     mostrarPresupuesto(presupuesto1);
   }
 }
 
-//buscamos el historial de presupuestos
-let listar = document.getElementById("listar");
-let historial = document.getElementById("historialPresupuestos");
+//Evento para borrar presupuesto
+eliminar.addEventListener("click", () => {
+  eliminarPresupuesto(historial.selectedIndex - 1, 1);
+  mostrarHistorial(arrayPresupuestos);
+});
 
+//Evento para mostrar presupuesto
+ver.addEventListener("click", () => {
+  traerPresupuesto(arrayPresupuestos[historial.selectedIndex - 1]);
+});
 
-//Capturamos para el botón Eliminar
-let eliminar = document.getElementById("eliminar");
-//evento para borrar presupuesto
-eliminar.addEventListener("click", ()=>{
-  console.log(arrayPresupuestos)
-  console.log(historial.selectedIndex-1)
-  eliminarPresupuesto(historial.selectedIndex-1, 1)
-  console.log(arrayPresupuestos)
-  mostrarHistorial(arrayPresupuestos)
-})
-//Capturamos para el botón VER
-let ver = document.getElementById("ver");
-//evento para mostrar presupuesto
-ver.addEventListener("click", ()=>{
+//Evento para resetear todo
+reset.addEventListener("click", () => {
+  localStorage.clear();
+  location.reload();
+});
 
-traerPresupuesto(arrayPresupuestos[historial.selectedIndex-1])
-})
-
-//capturamos boton restear para borrar todo
-let reset = document.getElementById("resetear");
-reset.addEventListener("click", ()=>{
-  localStorage.clear()
-  location. reload()
-})
-
-
-//mostramos el historial de presupuestos
-listar.addEventListener("click", ()=>{
- //guardamos en el array el storage.
-  if (localStorage.getItem("presupuestos")){
+//Mostramos el historial de presupuestos
+listar.addEventListener("click", () => {
+  //Guardamos en el array lo que haya en el storage, si es que hay algo.
+  if (localStorage.getItem("presupuestos")) {
     arrayPresupuestos = JSON.parse(localStorage.getItem("presupuestos"));
   }
-   
-  mostrarHistorial(arrayPresupuestos)
-})
+
+  mostrarHistorial(arrayPresupuestos);
+});
 
 function mostrarHistorial(arrayx) {
-  //reseteamos para que no duplique
-  historial.innerHTML = `<option value="0">Seleccionar...</option>`
+  //Reseteamos para que no duplique
+  historial.innerHTML = `<option value="0">Seleccionar...</option>`;
   for (const histo of arrayx) {
     let presuhistorial = document.createElement("option");
     presuhistorial.innerHTML += `<option value="${histo.numero}">Presupuesto n°${histo.numero} - Nombre: ${histo.nombre}</option>`;
-  historial.appendChild(presuhistorial);
+    historial.appendChild(presuhistorial);
   }
 }
 
 //función para eliminar presupuestos
-
-function eliminarPresupuesto(presupuesto, numero){
-  arrayPresupuestos.splice(presupuesto, numero)
-  console.log(arrayPresupuestos)
-  localStorage.setItem("presupuestos", JSON.stringify(arrayPresupuestos))
-  mostrarHistorial(arrayPresupuestos)
- 
+function eliminarPresupuesto(presupuesto, numero) {
+  arrayPresupuestos.splice(presupuesto, numero);
+  localStorage.setItem("presupuestos", JSON.stringify(arrayPresupuestos));
+  mostrarHistorial(arrayPresupuestos);
 }
 
-//mostramos el presupuesto ya hecho anteriormente
+//Mostramos el presupuesto ya hecho anteriormente
 function traerPresupuesto(presupuesto) {
   let presu = document.getElementById("divPresupuesto");
   let subTotal =
@@ -257,5 +263,5 @@ function mostrarPresupuesto(presupuesto) {
     presupuesto.mensualidad;
   let total = subTotal * 1.21;
   presu.innerHTML = `<p>Presupuesto n°: ${presupuestoNro}</p>\n<p>Nombre: ${presupuesto.nombre}</p>\n<p>Sistema: ${presupuesto.sistema}</p>\n<p>Tablets: ${presupuesto.tablet}</p>\n<p>Integración: ${presupuesto.integracion}</p>\n<p>PC adicionales: ${presupuesto.cantPc}</p>\n<p>Meses de abono: ${presupuesto.meses}</p>\n<p>Subtotal: $${subTotal} (Sin IVA)</p>\n<p>Total: $${total} (IVA incluído)</p>`;
-  localStorage.setItem("presupuestos", JSON.stringify(arrayPresupuestos))
+  localStorage.setItem("presupuestos", JSON.stringify(arrayPresupuestos));
 }
